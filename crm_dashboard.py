c76e2409af4748f9782c3b90a1f32dec87119a4f
oth3ym <dan.keith.forbes@gmail.com>
2025-07-29 17:02:06 -0400

    Initial commit

---
+++ crm_dashboard.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+
+DB_USER = "your_db_user"
+DB_PASS = "your_db_password"
+DB_HOST = "your_project.supabase.co"
+DB_PORT = "5432"
+DB_NAME = "postgres"
+
+DB_URL = f"postgresql://postgres:[YOUR-PASSWORD]@db.uhakcwvfbtrijvlttjno.supabase.co:5432/postgres"
+engine = create_engine(DB_URL)
