4c650be507efbccef523f72e972706611659244b
hadi <10430312+oth3ym@users.noreply.github.com>
2025-07-29 17:42:31 -0400

    Update crm_dashboard.py

--- crm_dashboard.py
+++ crm_dashboard.py
@@ -7,5 +7,5 @@ DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 
-DB_URL = f"postgresql://postgres:mzYH28tlOTskENy7@db.uhakcwvfbtrijvlttjno.supabase.co:5432/postgres"
+DB_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DB_URL)
